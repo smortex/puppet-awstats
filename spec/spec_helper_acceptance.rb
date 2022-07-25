b39e9863002a331fa4e2ba00fb727fb59c6e1929
Romain Tartière <romain@blogreen.org>
2022-07-24 18:31:52 -1000

    Autocorrect rubocop offenses

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,16 +1,14 @@
+# frozen_string_literal: true
+
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 
-unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
+unless (ENV['RS_PROVISION'] == 'no') || (ENV['BEAKER_provision'] == 'no')
   # This will install the latest available package on el and deb based
   # systems fail on windows and osx, and install via gem on other *nixes
-  foss_opts = {:default_action => 'gem_install'}
+  foss_opts = { default_action: 'gem_install' }
 
-  if default.is_pe?; then
-    install_pe;
-  else
-    install_puppet(foss_opts);
-  end
+  default.is_pe? ? install_pe : install_puppet(foss_opts)
 
   hosts.each do |host|
     on hosts, "mkdir -p #{host['distmoduledir']}"
@@ -27,11 +25,11 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'awstats')
+    puppet_module_install(source: proj_root, module_name: 'awstats')
     hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), {:acceptable_exit_codes => [0, 1]}
-      on host, puppet('module', 'install', 'stahnma-epel'), {:acceptable_exit_codes => [0, 1]}
-      on host, puppet('module', 'install', 'puppetlabs-apache'), {:acceptable_exit_codes => [0, 1]}
+      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { acceptable_exit_codes: [0, 1] }
+      on host, puppet('module', 'install', 'stahnma-epel'), { acceptable_exit_codes: [0, 1] }
+      on host, puppet('module', 'install', 'puppetlabs-apache'), { acceptable_exit_codes: [0, 1] }
     end
   end
 end
